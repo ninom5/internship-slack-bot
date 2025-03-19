5285286d5b899293101f95a73ac80494c2febd30
ninom5 <nmilas00@fesb.hr>
2025-03-19 14:44:11 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true, 
+    appToken: process.env.SLACK_APP_TOKEN,
+    port: process.env.PORT
+  });
+  app.message('hello', async ({ message, say }) => {
+    await say(`Hey there <@${message.user}>!`);
+  });
+  
+  (async () => {
+    await app.start();
+  
+    app.logger.info('⚡️ Bolt app is running!');
+  })();
